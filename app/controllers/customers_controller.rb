019f7d3d2c50468843bbb46db19e7c41a4a2fc15
Cezary Pietruszko <pietruszkocezary@gmail.com>
2021-03-14 19:30:53 +0100

    Sign into newsletter

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,14 @@
+class CustomersController < ApplicationController
+  def sign_into_newsletter
+    results = SignIntoNewsletter.new(email_param).call
+    render json: CustomerSerializer.new(results).serialized_json
+  rescue StandardError => e
+    render json: { error: e.message, status: 400 }, status: :bad_request
+  end
+
+  private
+
+  def email_param
+    params.from_jsonapi.require(:customer).require(:email)
+  end
+end
---
+++ app/services/sign_into_newsletter.rb
@@ -0,0 +1,11 @@
+class SignIntoNewsletter
+  def initialize(email)
+    @email = email
+  end
+
+  def call
+    raise Errors::InvalidEmailFormat unless @email.match(URI::MailTo::EMAIL_REGEXP)
+
+    Customer.where(email: @email).first_or_create!
+  end
+end
